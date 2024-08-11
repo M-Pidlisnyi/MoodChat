b0a913ed7db6e05b03de25d5a42de3eb292e0373
M-Pidlisnyi <mpidlisnyi88@gmail.com>
2024-08-11 22:05:07 +0300

    implemented sentiment analysis

---
+++ MoodChat/Services/CognitiveService.cs
@@ -0,0 +1,32 @@
+﻿using Azure;
+using Azure.AI.TextAnalytics;
+using Microsoft.EntityFrameworkCore.Metadata.Internal;
+namespace MoodChat.Services
+{
+
+
+    public class CognitiveService
+    {
+        private readonly AzureKeyCredential credentials;
+        private readonly Uri endpoint;
+        private readonly TextAnalyticsClient client; 
+        public CognitiveService(string key, string endpoint)
+        {
+            this.credentials = new AzureKeyCredential(key);
+            this.endpoint = new Uri(endpoint);
+            this.client = new TextAnalyticsClient(this.endpoint, this.credentials);
+        }
+        
+
+
+        public DocumentSentiment Analyze(string text)
+        {
+            var result = client.AnalyzeSentiment(text);
+
+            return result;
+        }
+    }
+
+
+
+}
